54e9dbfbea89f0c97c3ca282e9cba1b4ae3d653b
Julian Castaño <julian.catano@INFODEC.DEV>
2021-08-28 17:51:16 -0500

    Test add number rounds

--- real_effort_numbers_t_t/pages.py
+++ real_effort_numbers_t_t/pages.py
@@ -13,7 +13,7 @@ class AddNumbers(Page):
 
     def before_next_page(self):
         #Here's where the payoff is calculated
-        self.player.sub_rounds_stage_1 += 1
+        self.player.sub_rounds_stage_1 = 1 self.player.sub_rounds_stage_1
         self.player.total_sums = 1
         if self.player.sum_of_numbers == self.player.number_entered:
             self.player.payoff = Constants.payment_per_correct_answer
@@ -28,11 +28,11 @@ class AddNumbers(Page):
 
     def is_displayed(self):
         #Luego de que se acaba el tiempo, se salta las rondas (no las muestra) y va automáticamente a la siguiente página (Pagos).
-        self.player.sub_rounds_stage_1 += 1 #Aumentar las sub rondas en el stage 1
+        self.player.sub_rounds_stage_1 = self.player.sub_rounds_stage_1 + 1 #Aumentar las sub rondas en el stage 1
         if self.player.sub_rounds_stage_1 > 2: #cuando la subronda es 
             return False
         elif self.round_number <= Constants.num_rounds/2:
-            return True
+            return self.round
 
     def vars_for_template(self):
         number_1 = random.randint(1,100)
